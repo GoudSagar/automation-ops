aecc1f5c2d0448bb33a2472d19b7a85ff96cfb1a
GoudSagar <goud.sagar.t@gmail.com>
2026-05-28 03:48:06 +0000

    Ticket-0333 Updating transaction.sh script

--- transaction.sh
+++ transaction.sh
@@ -1,2 +1,6 @@
 A fork is a copy of a repository. Forking a repository allows you to freely experiment with changes without affecting the original project.
+5. git checkout: git checkout is used for switching the branch and also creating new branch from particular branch.
+
+command: local main> git fetch origin main  // "main" remote branch will bring changes to intermediatory branch "origin/main"
+         local main> git checkout -b release origin/main
 
